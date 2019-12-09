bf0762cf53ec377032a4132e386549cd3f0975a8
KMFidom <kimberleyfidom@msn.com>
2019-12-09 15:57:38 +0100

    Update postgresstogcp.py

--- dags/postgresstogcp.py
+++ dags/postgresstogcp.py
@@ -3,7 +3,7 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.contrib.operators.postgres_to_gcs_operator import PostgressToGoogleCloudStorageOperator
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
 
 args = {
