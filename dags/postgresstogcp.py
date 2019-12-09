2a36e2fb3a6b6b49a833cf12f08fc33699f8f6a7
Kimberley Fidom <kfidom@lmac-zc301lvdq.local.nl.bol.com>
2019-12-09 15:52:12 +0100

    test

---
+++ dags/postgresstogcp.py
@@ -0,0 +1,24 @@
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators.postgres_to_gcs_operator import PostgressToGoogleCloudStorageOperator
+
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+with DAG(
+    dag_id='dag_exercise-branching',
+    default_args=args,
+    schedule_interval='@daily',
+) as dag:
+
+    task1 = PostgressToGoogleCloudStorageOperator(postgres_conn_id='exercise_postgres',
+                                                  param_sql= 'select transfer_date from land_registry_price_paid_uk limit 10',
+                                                  bucket= 'example_postgresstogcp',
+                                                  filename= 'test'
+                                                  )
