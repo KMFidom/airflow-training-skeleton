38cbbbf13926996ecaab6c5d9cc4e58fd6898ba8
Kimberley Fidom <kfidom@lmac-zc301lvdq.local.nl.bol.com>
2019-12-10 16:06:08 +0100

    use case

---
+++ dags/Use_case.py
@@ -0,0 +1,27 @@
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from http_to_gcs_operator.py import HttpToGcsOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+with DAG(
+    dag_id='use_case',
+    default_args=args,
+    schedule_interval='@daily',
+) as dag:
+
+    connect_api = HttpToGcsOperator(
+        task_id='connect_api',
+        http_conn_id = 'https://api.exchangeratesapi.io/',
+        endpoint = 'history?start_at=2018-01-01&end_at=2018-01-02&symbols=EUR&base=GBP',
+        gcs_path = 'gs://use_case_airflow'
+    )
+
+    connect_api
