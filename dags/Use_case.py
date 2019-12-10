3ca6fd16d0147164f318d51dffceb2b588e23d7c
Kimberley Fidom <kfidom@lmac-zc301lvdq.local.nl.bol.com>
2019-12-10 16:10:23 +0100

    use case

--- dags/Use_case.py
+++ dags/Use_case.py
@@ -4,7 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from http_to_gcs_operator.py import HttpToGcsOperator
+from operator.http_to_gcs_operator import HttpToGcsOperator
 
 args = {
     'owner': 'Airflow',
